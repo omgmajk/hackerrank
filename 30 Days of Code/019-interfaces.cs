b78ad01e197598efaa43a0c1d1e9a9fd44e4c32e
Michael Bäck <majkieboy@gmail.com>
2021-07-22 10:45:50 -0400

    Added day 19.

---
+++ 30 Days of Code/019-interfaces.cs
@@ -0,0 +1,15 @@
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        int total = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if(n % i == 0)
+            {
+                total += i;
+            }
+        }
+        return total;
+    }
+}
